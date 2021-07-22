97df2379c387ad534caa5457ca60802fbc6e32e0
Sean <simpson.sean@outlook.com>
2021-07-22 11:57:29 -0700

    WORKDONE: Intial Passing Test plus passing GET test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -38,4 +38,17 @@ describe('03_separation-of-concerns-demo routes', () => {
   //       expect(res.body).toEqual(order);
   //     });
   // });
+
+  it('gets all the orders', async () => {
+    const order = await Order.insert({ quantity: 10 });
+
+    return request(app)
+      .get(`/api/v1/orders/`)
+      .then((res) => {
+        expect(res.body).toEqual([order]);
+      });
+  });
+
+
+
 });
